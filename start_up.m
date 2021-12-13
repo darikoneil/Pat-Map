6f8e5c53ddac7ea8268a9f2942aaa775c9a6a909
darikoneil <49213119+darikoneil@users.noreply.github.com>
2021-12-13 13:06:27 -0500

    Add files via upload

--- start_up.m
+++ start_up.m
@@ -1,27 +1,36 @@
 function [basepath,params] = start_up()
 
+% Darik O'Neil 12-13-2021 Rafael Yuste Laboratory
+
+%Purpose: This function initializes the set of parameters and  locates the data
+%and dependencies.
+
+%To accomplish this we do three steps
+
+%(1, Establish Paths)
+%(2, Data Import)
+%(3, Initialize Parameters)
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%ESTABLISH PATHS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-basepath = pwd;
+basepath = pwd; %set basepath
 
 % THIRDPARTY DEPENDENCIES
-addpath(fullfile(basepath,'thirdparty'))
-addpath(fullfile(basepath,'thirdparty','QPBO-v1.32.src'))
-addpath(fullfile(basepath,'thirdparty','glmnet_matlab'))
-addpath(fullfile(basepath,'thirdparty','glmnet_matlab','glmnet_matlab'))
-addpath(fullfile(basepath,'expt'))
+addpath(fullfile(basepath,'thirdparty'));
+addpath(fullfile(basepath,'thirdparty','QPBO-v1.32.src')); %Solver implementation
+addpath(fullfile(basepath,'thirdparty','glmnet_matlab'));
+addpath(fullfile(basepath,'thirdparty','glmnet_matlab','glmnet_matlab')); %GLM implementation
+addpath(fullfile(basepath,'thirdpart','parwaitbar-master')); %Wait Bar Interface for Parallel Processing
 
 % SOURCE FUNCTIONS
-
-addpath(fullfile(basepath,'src_fun'))
-addpath(fullfile(basepath,'src_fun','ANALYSIS'))
-addpath(fullfile(basepath,'src_fun','FRAMEWORK'))
-addpath(fullfile(basepath,'src_fun','MLE_STRUC'))
-addpath(fullfile(basepath,'src_fun','include'))
-addpath(fullfile(basepath,'src_fun','STRUCTURE'))
-
+addpath(fullfile(basepath,'src_fun'));
+addpath(fullfile(basepath,'src_fun','ANALYSIS'));
+addpath(fullfile(basepath,'src_fun','FRAMEWORK'));
+addpath(fullfile(basepath,'src_fun','MLE_STRUC'));
+addpath(fullfile(basepath,'src_fun','include')); %Is this archaic or do we still need this even though everything was already cooked?
+addpath(fullfile(basepath,'src_fun','STRUCTURE'));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%DATA IMPORT%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
