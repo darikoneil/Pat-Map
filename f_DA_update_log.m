4dc254f7933d32fa2adb482b8a50a29e433d74bd
darikoneil <49213119+darikoneil@users.noreply.github.com>
2021-12-22 15:33:49 -0500

    Delete f_DA_update_log.m

--- f_DA_update_log.m
+++
@@ -1,6 +0,0 @@
-function f_DA_update_log(app, new_text)
-    app.log = [sprintf('%d : %s\n', app.log_line, new_text) app.log ];
-    app.LogTextArea.Value = app.log;
-    app.log_line = app.log_line + 1;
-    drawnow; pause(0.05);
-end
