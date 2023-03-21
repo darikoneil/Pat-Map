3523d2a523ce0106e9cd70aa146275885375473d
darikoneil <d.oneil@columbia.edu>
2023-03-21 13:26:26 -0400

    add updated panels 1/2/3 + some janky matlab test functions

--- SourceFun/AppFun/Utilities/f_DA_update_learned_models.m
+++
@@ -1,5 +0,0 @@
-function f_DA_update_learned_models(app)
-
-app.TotalLearnedModelsEditField.Value = length(app.model_collection.models);
-
-end
--- src/code/parameter_estimation/estimate_parameters_implementation_specific_call.m
+++ src/code/parameter_estimation/estimate_parameters_implementation_specific_call.m
@@ -7,28 +7,27 @@ function estimate_parameters_implementation_specific_call(app)
 app.params.stage=3; %parameter estimation in progress
 
 % shuttle to the appropriate function
-switch app.params.implementationMode
+switch app.params.implementation_mode
     case 1
-        f_DA_update_log(app, 'Default Implementation Selected');
-        f_DA_update_log(app, 'Beginning Parameter Estimation');
+        update_log(app, 'Default Implementation Selected');
+        update_log(app, 'Beginning Parameter Estimation');
         [app.best_model,app.model_collection,app.params] = estimate_parameters_collection_implementation(app.params,app.models);
-        f_DA_update_log(app, 'Finished Parameter Estimation');
+        update_log(app, 'Finished Parameter Estimation');
     case 2
+        fprintf('NOT IMPLEMENTED');
         [app.best_model,app.model_collection,app.params] = estimateParametersChunked(app.params, app.models);
     case 3
-        f_DA_update_log(app, 'Series Implementation Selected');
-        f_DA_update_log(app, 'Beginning Parameter Estimation');
+        update_log(app, 'Series Implementation Selected');
+        update_log(app, 'Beginning Parameter Estimation');
         [app.best_model,app.model_collection,app.params] = estimate_parameters_series_implementation(app.params, app.models, app.model_collection);
-        f_DA_update_log(app, 'Finished Parameter Estimation');
+        update_log(app, 'Finished Parameter Estimation');
     case 4
-        f_DA_update_log(app, 'Parallel Implementation Selected');
-        f_DA_update_log(app, 'Beginning Parameter Estimation');
+        update_log(app, 'Parallel Implementation Selected');
+        update_log(app, 'Beginning Parameter Estimation');
         [app.best_model,app.model_collection,app.params] = estimate_parameters_parallel_implementation(app.params, app.models, app.model_collection);
-        f_DA_update_log(app, 'Finished Parameter Estimation');
+        update_log(app, 'Finished Parameter Estimation');
 end
 
 app.params.stage=5;
-f_DA_update_learned_models(app);
-f_DA_update_unlearned_models(app);
 
 end
