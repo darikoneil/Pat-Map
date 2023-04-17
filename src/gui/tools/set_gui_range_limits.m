58c5f53064d3bc3f4b5ec6ada7c58e3ac84370f4
darikoneil <d.oneil@columbia.edu>
2023-04-17 12:36:42 -0400

    D) Run Linting / Test Suite 1) Run Linter 2) Run Test Suite 3) Run OS Runners 4) Run Matlab Version Runners 5) launch test runners for matlab versions

--- src/gui/tools/set_gui_range_limits.m
+++ src/gui/tools/set_gui_range_limits.m
@@ -4,7 +4,7 @@ num_udf = app.params.num_udf;
 app.StimulusEM.Limits = [0 num_udf];
 app.StimulusEditField.Limits = [0 num_udf];
 app.StimulusEV.Limits=[0 num_udf];
-app.StimulusPCN.Limits=[0 num_udf];
+app.Stimulus_PCN.Limits=[0 num_udf];
 
 num_neurons = app.params.num_neurons;
 app.HighlightedNode.Limits = [-1 num_neurons];
