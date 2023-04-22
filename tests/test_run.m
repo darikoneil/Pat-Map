e5c3481e03bd34ff0cf54dea81658b64063ae356
Darik A O’Neil <d.oneil@columbia.edu>
2023-04-22 13:24:42 -0400

    Add files via upload

--- tests/test_run.m
+++ tests/test_run.m
@@ -1,6 +1,17 @@
-app = test_panel_1('shutin');
 
-app = test_panel_2(app);
+app = pat_map();
 
-app = test_panel_3(app);
+dataset = 'example_datasets/drifting_gratings/visual_cortex';
+default_path = pwd;
 
+app.file_data = strcat(default_path, '/', dataset, '_data.mat');
+app.file_udf = strcat(default_path, '/', dataset, '_udf.mat');
+app.file_rois = strcat(default_path, '/', dataset, '_rois.mat');
+app.file_params = strcat(default_path, '/', dataset, '_params.mat');
+
+load_data_button_pushed(app);
+load_udf_button_pushed(app);
+load_rois_button_pushed(app);
+load_params_button_pushed(app);
+
+wrapped_run_all(app);
