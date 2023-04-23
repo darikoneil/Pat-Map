4576f8ce4a7c09ce1d4d44511ce1639f0ef8c6b3
Darik A O’Neil <d.oneil@columbia.edu>
2023-04-23 08:57:20 -0400

    Update pat_map.m

--- pat_map.m
+++ pat_map.m
@@ -4,16 +4,16 @@ function [app] = pat_map()
 basepath = pwd; %current folder
 
 % Add source functions
-addpath(genpath(strcat(basepath,'\src')));
+addpath(genpath(strcat(basepath,'/src')));
 
 % example datasets
-addpath(genpath(strcat(basepath,'\example_datasets')));
+addpath(genpath(strcat(basepath,'/example_datasets')));
 
 % test suite
-addpath(genpath(strcat(basepath,'\tests')));
+addpath(genpath(strcat(basepath,'/tests')));
 
 % Add experiments
-addpath(strcat(basepath, '\experiments')); %ONLY FOLDER NOT SUBS
+addpath(strcat(basepath, '/experiments')); %ONLY FOLDER NOT SUBS
 
 app = PatMap();
 
